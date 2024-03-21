07fc51f40971d3c1513e6be4ac00d815fe9023a4
Ola Juul Holm <olajho@ulrik.uio.no>
2024-03-21 19:46:03 +0100

    created the controls class

---
+++ controls.js
@@ -0,0 +1,34 @@
+class Controls {
+    constructor(){
+        // Directions
+        this.forward=false;
+        this.left=false;
+        this.right=false;
+        this.reverse=false;
+
+        this.#addKeyboardListeners();
+    }
+
+    /** 
+     * In Javascript the '#' symbol before a method is used to declare the method as private, 
+     * meaning it cannot be accessed outside this class 
+     * */
+    #addKeyboardListeners(){
+        document.onkeydown = (event) => {
+            switch(event.key) {
+                case "ArrowLeft":
+                    this.left = true;
+                    break;
+                case "ArrowRight":
+                    this.right = true;
+                    break;
+                case "ArrowUp":
+                    this.forward = true;
+                    break;
+                case "ArrowDown":
+                    this.reverse = true;
+                    break;
+            }
+        }
+    }
+}

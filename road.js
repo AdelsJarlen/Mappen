9385d286c581c901e194846c2fcdc10db928052d
Ola Juul Holm <olajho@ulrik.uio.no>
2024-03-21 20:52:42 +0100

    drawing multiple lanes

--- road.js
+++ road.js
@@ -2,7 +2,7 @@ class Road {
     constructor(x,width,laneCount = 3) {
         this.x = x;
         this.width = width;
-        this.laneCount = this.laneCount;
+        this.laneCount = laneCount;
 
         this.left = x - width / 2;
         this.right = x + width / 2;
@@ -17,28 +17,17 @@ class Road {
         ctx.strokeStyle = "White";
 
         for(let i = 0; i <= this.laneCount; i++) {
-            // linear interpolation
-            /**
-             * function lerp(A,B,t) {
-             *      return A + (B - A) * t;
-             * }
-             */
-
             const x = lerp(
                 this.left,
                 this.right,
                 i / this.laneCount
             );
-        }
-        ctx.beginPath();
-        ctx.moveTo(this.left,this.top);
-        ctx.lineTo(this.left,this.bottom);
-        ctx.stroke();
 
-        ctx.beginPath();
-        ctx.moveTo(this.right,this.top);
-        ctx.lineTo(this.right,this.bottom);
-        ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(x,this.top);
+            ctx.lineTo(x,this.bottom);
+            ctx.stroke();
+        }
     }
 }
 
